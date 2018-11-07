d7d14a0d92a69cf8ce1d489081ea228d5ae01034
Luis Alberto <disconluis@gmail.com>
2018-11-07 11:03:57 +0100

    creado la pagina galeria

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+
+//variables 
+let imagenes = document.querySelectorAll('.gallery__img')
+let modal = document.querySelector('#modal')
+let img = document.querySelector('#modal__img')
+let boton = document.querySelector('#modal__boton')
+
+for (let i = 0; i < imagenes.length; i++){
+    imagenes[i].addEventListener('click', e => {
+        modal.classList.toggle('modal--open')
+        let src = e.target.src
+        img.setAttribute('src', src)
+    })
+}
+
+boton.addEventListener('click', () =>{
+    modal.classList.toggle('modal--open')
+})
